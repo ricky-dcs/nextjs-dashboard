c0acbdaca96409eae98255fa405f08336c767986
Ricky Lau <ricky.lau@customerservice.nsw.gov.au>
2026-05-21 15:33:19 +1000

    added second font Lusitana to local

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+import { inter,lusitana } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -8,7 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+
+      <body className={`${inter.className} antialiased`}>{children}
+        <h1 className={`${lusitana}.className text-xl text-gray-800 md:text-3xl md:leading-normal`}>Hello world!</h1>
+        <p className={`${lusitana}.className font-normal`}>this is the body copy</p>
+      </body>
     </html>
   );
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,19 @@
 import {Inter} from 'next/font/google';
+import Lusitana from 'next/font/local'
+import type { AppProps } from 'next/app'
+
 
 export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({
+  src: [
+    {
+      path: './fonts/Lusitana-Regular.ttf', // <-- use the correct relative path
+      weight: '400',
+    },
+    {
+      path: './fonts/Lusitana-Bold.ttf',
+      weight: '700',
+    },
+  ],
+})
+
