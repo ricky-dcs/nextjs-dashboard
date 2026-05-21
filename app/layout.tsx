72424e0dd7eafe7a6412a1337d82dcd003b82ca3
Ricky Lau <ricky.lau@customerservice.nsw.gov.au>
2026-05-21 16:36:11 +1000

    refactor layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import { inter,lusitana } from '@/app/ui/fonts';
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -10,8 +10,6 @@ export default function RootLayout({
     <html lang="en">
 
       <body className={`${inter.className} antialiased`}>{children}
-        <h1 className={`${lusitana}.className text-xl text-gray-800 md:text-3xl md:leading-normal`}>Hello world!</h1>
-        <p className={`${lusitana}.className font-normal`}>this is the body copy</p>
       </body>
     </html>
   );
