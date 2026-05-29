986acf9f747415a07ba8898e325eb72e9c320147
Ricky Lau <ricky.lau@customerservice.nsw.gov.au>
2026-05-29 13:20:14 +1000

    added skeleton for cards

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,16 +14,8 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper({
-  totalPaidInvoices,
-  totalPendingInvoices,
-  numberOfInvoices,
-  numberOfCustomers }: {
-    totalPaidInvoices: number;
-    totalPendingInvoices: number;
-    numberOfInvoices: number;
-    numberOfCustomers: number;
-  }) {
+export default async function CardWrapper() {
+  const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers }= await fetchCardData();
 
   return (
     <>
