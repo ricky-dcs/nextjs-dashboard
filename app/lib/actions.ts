32f229c8e1b9ef206b7f44180fcb0a0c1478f560
Ricky Lau <ricky.lau@customerservice.nsw.gov.au>
2026-06-04 16:48:08 +1000

    revalidate and redirect path

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require'});
@@ -29,4 +31,6 @@ export async function createInvoice(formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 
+  revalidatePath('dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
