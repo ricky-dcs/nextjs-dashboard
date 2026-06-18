007108219c587189864fbae978dcdb2348167964
Ricky Lau <ricky.lau@customerservice.nsw.gov.au>
2026-06-18 15:39:07 +1000

    added meta page titles to various pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,10 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 import { Suspense } from 'react';
+
+export const metadata: MetaData = {
+  title: 'Login',
+};
  
 export default function LoginPage() {
   return (
