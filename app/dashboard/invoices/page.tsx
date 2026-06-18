5653719047fee77acd58202ed97ab1674cd85677
Ricky Lau <ricky.lau@customerservice.nsw.gov.au>
2026-06-18 15:09:14 +1000

    added title template to layout.tsx

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,10 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
+
+export const metadata: MetaData = {
+  title: 'Invoices',
+};
  
 export default async function Page(
   props: {
